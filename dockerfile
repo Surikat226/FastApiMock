cac5da299d68927d92219206015773d3a0df5940
Surikat228 <awf2014@yandex.ru>
2024-11-15 23:01:29 +0300

    minor upd

--- dockerfile
+++ dockerfile
@@ -1,1 +1,9 @@
 FROM python:3.12-slim
+
+WORKDIR /app
+ 
+COPY . .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+ENTRYPOINT [ "uvicorn", "main:app", "port", "8000", "--host", "127.0.0.1" ]
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from external_data import Users
 
 app = FastAPI()
-user = Users()
+users = Users()
 
 
 @app.get("/")
@@ -12,10 +12,10 @@ async def root():
 
 @app.post("/users/create")
 async def create_user(name: str, last_name: str):
-    user.save_user(name, last_name)
+    users.save_user(name, last_name)
     return {"message": f"Successfully created user: {name + ' ' + last_name}"}
 
 
 @app.get("/users/list")
 async def show_users_list():
-    return {"users": user.get_users()}
+    return {"users": users.get_users()}
